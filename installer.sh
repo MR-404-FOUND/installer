527776a0c214edfd01e9893b077b4e7a43c29f03
Mr.404_FouNd <52834382+MR-404-FOUND@users.noreply.github.com>
2019-07-13 16:42:53 +0700

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,58 @@
+clear
+sleep 1
+clear
+
+echo "
+██████╗ ██╗███████╗██╗ ██████╗ 
+██╔══██╗██║╚══███╔╝██║██╔═══██╗
+██████╔╝██║  ███╔╝ ██║██║   ██║
+██╔══██╗██║ ███╔╝  ██║██║▄▄ ██║
+██║  ██║██║███████╗██║╚██████╔╝
+╚═╝  ╚═╝╚═╝╚══════╝╚═╝ ╚══▀▀═╝"
+sleep 2
+echo "\033[34;1mini tools buat instal eak"
+sleep 1
+echo "===================="
+echo "\033[34;1m[01] instal" 
+echo "===================="
+read -p "#Mr.404_FouNd ~>#" pilihan
+
+
+if [ $pilihan = "01" ] || [ $pilihan = "01" ]
+then
+apt update
+apt upgrade
+pkg install git
+pkg install python2
+pkg install wget
+pkg install curl
+gem install lolcat
+pkg install cowsay
+pkg install vim
+pkg install nano
+pkg install toilet
+pkg install figlet
+pkg install ruby
+pip2 install mechanize
+pip2 install requests
+pkg install php
+pkg install neofetch
+pkg install screenfetch
+pkg install mc
+pkg install sl
+pkg install cmatrix
+pkg install tree
+pkg install mpv
+pkg install w3m
+pkg install termux-api
+pip install mps_youtube
+pip install scapy
+pkg install moon-buggy
+pkg install openssl
+pkg install libcurl
+pip2 install urllib3 chardet certifi idna requests
+pkg install clang
+pkg install proot
+echo "SELESAI"
+
+fi
